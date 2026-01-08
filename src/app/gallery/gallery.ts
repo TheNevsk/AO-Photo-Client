2c3c8d0debd8973315eaceed1b94215985cc6379
aviram <aviram>
2026-01-08 22:05:55 +0200

    Gallery photo pop-up

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, HostListener} from '@angular/core';
 import {ActivatedRoute, RouterLink} from '@angular/router';
 import {BackendService} from '../../shared/services/backend-service';
 
@@ -9,8 +9,10 @@ import {BackendService} from '../../shared/services/backend-service';
   styleUrl: './gallery.css',
 })
 export class Gallery {
+  private galleryPath!: string;
   name!: string;
   photoCount: number = 0;
+  selectedPhoto: string | null = null;
   constructor(private route: ActivatedRoute, private server: BackendService) {}
 
   ngOnInit() {
@@ -18,11 +20,26 @@ export class Gallery {
       this.name = params.get('name')!;
       console.log("name is " + this.name);
 
+      this.galleryPath = "images/"+ this.name + "/";
       this.photoCount = this.server.getGalleryPhotoCount(this.name);
     });
   }
 
   get photoIndexes(): number[] {
     return Array.from({ length: this.photoCount }, (_, i) => i);
+  }
+
+  //pop-up for clicked pjoto
+  open(i: number) {
+    this.selectedPhoto = this.galleryPath + this.name + i + ".PNG";
+  }
+
+  close() {
+    this.selectedPhoto = null;
+  }
+
+  @HostListener('document:keydown.escape')
+  onEsc() {
+    this.close();
   }
 }
