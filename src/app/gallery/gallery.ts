afb539a8f862f7adf0a63ac33695ccab673e0151
aviram <aviram>
2026-01-08 16:47:12 +0200

    Brands demo gallery - rough first take

--- src/app/gallery/gallery.ts
+++ src/app/gallery/gallery.ts
@@ -1,20 +1,28 @@
 import {Component} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, RouterLink} from '@angular/router';
+import {BackendService} from '../../shared/services/backend-service';
 
 @Component({
   selector: 'app-gallery',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './gallery.html',
   styleUrl: './gallery.css',
 })
 export class Gallery {
   name!: string;
-  constructor(private route: ActivatedRoute) {}
+  photoCount: number = 0;
+  constructor(private route: ActivatedRoute, private server: BackendService) {}
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       this.name = params.get('name')!;
       console.log("name is " + this.name);
+
+      this.photoCount = this.server.getGalleryPhotoCount(this.name);
     });
+  }
+
+  get photoIndexes(): number[] {
+    return Array.from({ length: this.photoCount }, (_, i) => i);
   }
 }
